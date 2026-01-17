5b491d26c83f4b9ce826b0547ade35dd60996578
ascepecindia <asce.pec.india@gmail.com>
2026-01-17 22:10:32 +0530

    REMOVING ERROR

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import './index.css';
-import { HelmetProvider } from 'react-helmet-async'; // <--- Import this
+import { HelmetProvider } from 'react-helmet-async';
+
+// --- CORRECTED CSS IMPORT PATHS ---
+// Based on your screenshot, index.css is inside the 'styles' folder
+import './styles/index.css'; 
+// If you are using tailwind.css separately, import it here too:
+import './styles/tailwind.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider> {/* <--- Wrap App with this */}
+    <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
